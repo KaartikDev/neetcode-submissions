f8ff750c8cc3de3dd7199f742ca2c11410cf590f
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-06-28 01:45:04 -0700

    Add: single-number - submission-5

---
+++ Data Structures & Algorithms/single-number/submission-5.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # all 0 if two identical number zero'd
+        base = 0
+        for num in nums:
+            base^=num
+        return base
