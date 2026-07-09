5f9874548db11865aea2b917dba2fd4231fe1964
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-07-09 14:33:55 -0700

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        #(m-1)+(n-1) choose n-1
+
+        def factorial(i):
+            if i <= 1:
+                return 1
+            else:
+                return i * factorial(i-1)
+        
+        net_fact = factorial(n+m-2)
+        row_fact = factorial(m-1)
+        col_fact = factorial(n-1)
+
+        return net_fact//(row_fact*col_fact)
+
+        # O(m+n) time
+        # O(m+n) space
