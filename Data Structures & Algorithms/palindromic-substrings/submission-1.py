c8f54e820e0f8972a81b34a7196df350325ed0ad
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-07-09 18:01:20 -0700

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        #top down recrusive strategy 
+        # dp(l,r) is palindrome if dp(l+1,r-1) is palindrome and and s[l] == s[r-1] 
+
+        if len(s) <= 1:
+            return 1
+
+        memo = {}
+        def dp(l,r):
+            #l is inclusive
+            #r is exlcusive
+            # if l < 0 or r < 0 or l >= len(s) or r > len(s)+1:
+            #     return False
+            
+            if r - l <= 1:
+                return True
+            if (l,r) in memo:
+                return memo[(l,r)]
+            
+            memo[(l,r)] = s[l] == s[r-1] and dp(l+1,r-1)
+            return memo[(l,r)]
+        
+        count = 0
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if dp(i,j):
+                    count+=1
+        # print(memo)
+        return count
+            
+        
+        
+        
+        
+        #brute force 
+        #make set of all substr --> O(n^2) subtrings each O(n) long
+        #check isPalin on all O(n^2) subtrings --> O(n^3) time, O(n^3) space 
