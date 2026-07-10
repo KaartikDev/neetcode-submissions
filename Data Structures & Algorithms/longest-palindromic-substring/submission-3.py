dc292ed228d5391ad7e3628feface95a0818df08
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-07-09 17:09:03 -0700

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+                return s
+        
+        def isPalin(s):
+            l = 0
+            r = len(s)-1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+
+        #one sol: build all possible substrings
+        #check for longest one
+        subStrSet = set()
+        for i in range(len(s)):
+            for j in range(i,len(s)+1):
+                   subStrSet.add(s[i:j])
+        # print(subStrSet)   
+
+        best = ""
+        for curr in subStrSet:
+            if isPalin(curr) and len(curr) > len(best):
+                best = curr
+        return best
