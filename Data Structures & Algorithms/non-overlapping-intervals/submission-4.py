42b6acd894000082d33338e25446faa8476d1a5d
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-08-02 01:32:01 -0700

    Add: non-overlapping-intervals - submission-4

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        #how do yk which of two intervalls to remove?
+
+        #remove the ones that ends later
+
+        heap = intervals.copy()
+        heapq.heapify(heap)
+
+        stack = []
+        count = 0
+
+        while heap:
+            curr = heapq.heappop(heap)
+
+            if not stack:
+                stack.append(curr)
+                continue
+            
+
+            if stack[-1][1] <= curr[0]:
+                stack.append(curr)
+            else:
+                count+=1
+                #keep the interval that ends earlier
+                if stack[-1][1] >= curr[1]:
+                    stack.pop()
+                    stack.append(curr)
+                #else do nothing
+                
+        return count
+
+            
