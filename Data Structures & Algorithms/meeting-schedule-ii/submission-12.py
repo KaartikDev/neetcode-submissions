a12d820c1869f5c072646e665c7a0f71b646821e
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-08-03 01:17:32 -0700

    Add: meeting-schedule-ii - submission-12

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-12.py
@@ -0,0 +1,39 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if len(intervals) == 0:
+            return 0
+        
+        #find depth
+        conflicts = 1
+
+        heapByStart = [(i.start,i.end) for i in intervals]
+        heapq.heapify(heapByStart) #sort by start times ascending
+
+        heapByEnd = []
+
+        while heapByStart:
+            currStart,currEnd = heapq.heappop(heapByStart)
+            # print(curr,queue)
+            
+            #get rid of any intervals that have now ended
+            #heap by end is ordered with intervals with earliest expiry times first
+            while heapByEnd and heapByEnd[0] <= currStart:
+                # print("expired", heapByEnd[-1], "during", [currStart,currEnd])
+                heapq.heappop(heapByEnd)
+
+            heapq.heappush(heapByEnd,currEnd)
+            # print(heapByEnd[-1])
+            conflicts = max(conflicts,len(heapByEnd))
+            
+        
+        return conflicts
+
+        #time we have n intervals, each does logn inseartion -> O(nlogn)
