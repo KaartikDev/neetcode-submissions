8c714fc4da693e8ef9ce6f03abbf80e8b25c580f
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-08-09 18:32:18 -0700

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+
+        if not strs:
+            return prefix
+
+        for i in range(len(strs[0])):
+            #check if charcter belongs for every word in list
+            for word in strs:
+                #if our next prefix is too long for current word or charcter mismatch
+                if i >= len(word) or word[i] != strs[0][i]:
+                    return prefix
+            
+            prefix+=strs[0][i]
+        
+        #entire word at index 0 was was used
+        return prefix
+
+
+        
+        
