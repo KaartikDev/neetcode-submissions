b438d362d722bbe8eceeaf2f5d709e269707f2ea
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-08-09 18:40:24 -0700

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        #we just do what a bubble sort?
+        l = 0
+        r = len(nums)-1
+        
+        count = 0
+
+        while r >= 0 and nums[r] == val:
+            count+=1
+            r-=1
+        
+        
+        
+        while l < r:
+            if nums[l] == val:
+                nums[l] = nums[r]
+                nums[r] = val
+                
+                while r >= 0 and nums[r] == val:
+                    count+=1
+                    r-=1
+            
+            l+=1
+
+        # print(nums, count)
+        return len(nums)- count
+        
+        
