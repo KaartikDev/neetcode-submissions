ef94b23b8d1e3abaf3c21381e8d955135d338d5f
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-08-21 17:25:24 -0700

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left = 0
+        right = x
+        valid_res = 0
+        # print("x=",x)
+        while left <= right:
+            mid = (left+right)//2
+            # print("l=",left,"r=",right,"mid=",mid,"mid_sq=",mid*mid)
+
+            if (mid*mid) > x:
+                right = mid-1
+            else:
+                valid_res = mid
+                left = mid + 1
+        
+        return valid_res
