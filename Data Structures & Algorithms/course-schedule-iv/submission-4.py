66ac9e266db947e91b419c19691565de151607a8
Ktej <128007800+KaartikDev@users.noreply.github.com>
2026-09-03 01:34:48 -0700

    Add: course-schedule-iv - submission-4

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-4.py
@@ -0,0 +1,39 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        #we need to add all of the parents
+
+        #so if bi --> ai and later we have ci --> bi
+        #this means bi depends on ai and ci depends on bi, and by deifiton ci dpeends on ai
+        
+        #for querries we just need to reverse this, and then check if we can traverse vj to uj.
+        #Time: O(Q * (V+E)) as we do dfs for each search
+
+        adjMap = {}
+        
+        for u,v in prerequisites:
+            if v not in adjMap:
+                adjMap[v] = []
+            adjMap[v].append(u)
+        
+        print(adjMap)
+        memo = {}
+        def is_prereq(u,v):
+            if u == v:
+                return True
+            if (u,v) in memo:
+                return memo[(u,v)]
+            
+            for dependancy in adjMap.get(u,[]):
+                    if is_prereq(dependancy,v):
+                        memo[(u,v)] = True
+                        return True
+            memo[(u,v)] = False
+            return False
+                
+        
+        res = []
+        for target, start in queries:
+            res.append(is_prereq(start,target))
+        # print(memo)
+        return res
+
